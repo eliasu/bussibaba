219cb3b79f78074fbb06c4374708d5269956da0b
Eliasu <elias@commandg.de>
2026-02-13 02:21:51 +0100

    working gallery

--- resources/js/site.js
+++ resources/js/site.js
@@ -1,1 +1,10 @@
-// This is all you.
+import GalleryApp from './gallery/index.js';
+
+// Initialize gallery when DOM is ready
+if (document.readyState === 'loading') {
+  document.addEventListener('DOMContentLoaded', () => {
+    new GalleryApp();
+  });
+} else {
+  new GalleryApp();
+}
